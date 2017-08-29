4cc77fc013ed1f11b94e5f83f7a787c8cc900d28
jkennyh <kenny.rabaye@gmail.com>
2017-08-29 17:53:57 +0200

    Commit

---
+++ src/BasicEs004Main.java
@@ -0,0 +1,19 @@
+
+public class BasicEs004Main {
+
+	public static void main(String[] args) {
+		
+		int result = -5 + (8 * 6);			// -5 + 8 * 6
+		System.out.println(result);
+		
+		result = (55 + 9) % 9;
+		System.out.println(result);
+		
+		result = 20 + (-3 * 5) / 8;			// 20 + -3 * 5 / 8
+		System.out.println(result);
+		
+		result = 5 + (15 / 3) * 2 - (8 % 3);	// 5 + 15 / 3 * 2 - 8 % 3
+		System.out.println(result);
+	}
+
+}
